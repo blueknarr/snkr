9bcadfb6fa950e67b3e447e7df7b1549ae5e5e10
blueknarr <knarrblue@gmail.com>
2023-09-11 19:00:31 +0900

    feat: upcoming use case

---
+++ lib/domain/use_case/get_upcoming_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:snkr/domain/model/upcoming.dart';
+import 'package:snkr/domain/repository/upcoming_repository.dart';
+
+import '../../core/result/result.dart';
+
+class GetUpcomingUseCase {
+  final UpcomingRepository repository;
+
+  GetUpcomingUseCase(this.repository);
+
+  Future<Result<List<Upcoming>>> execute() async {
+    try {
+      final upcoming = await repository.getUpcomingProducts();
+      return Result.success(upcoming);
+    } catch (e) {
+      return Result.error('$e');
+    }
+  }
+}
