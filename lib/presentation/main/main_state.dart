8a878d19f350c0e28e091ae61667c2a87162c758
blueknarr <knarrblue@gmail.com>
2023-09-12 11:54:18 +0900

    feat: product, event, isLoading state 및 view model 추가

---
+++ lib/presentation/main/main_state.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../domain/model/event.dart';
+import '../../domain/model/product.dart';
+
+part 'main_state.freezed.dart';
+
+part 'main_state.g.dart';
+
+@freezed
+class MainState with _$MainState {
+  const factory MainState({
+    @Default([]) List<Product> products,
+    @Default([]) List<Event> events,
+    @Default(false) bool isLoading,
+    @Default(0) int appBarIndex,
+  }) = _MainState;
+
+  factory MainState.fromJson(Map<String, Object?> json) =>
+      _$MainStateFromJson(json);
+}
---
+++ lib/presentation/main/main_view_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:snkr/core/result/result.dart';
+import 'package:snkr/domain/use_case/get_products_use_case.dart';
+import 'package:snkr/presentation/main/main_state.dart';
+
+import '../../domain/use_case/get_event_use_case.dart';
+
+class MainViewModel with ChangeNotifier {
+  final GetProductsUseCase _getProductsUseCase;
+  //final GetEventUseCase _getEventUseCase;
+
+  MainViewModel(this._getProductsUseCase) {
+    fetchProducts();
+  }
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  Future<void> fetchProducts() async {
+    final products = await _getProductsUseCase.execute();
+
+    switch (products) {
+      case Success(:final data):
+        _state = state.copyWith(
+          products: data,
+          isLoading: true,
+        );
+        notifyListeners();
+      case Error(:final e):
+        _state = state.copyWith();
+    }
+  }
+
+  void changeAppBarIndex(String menu) {
+    Map<String, int> appBarIndex = {'Feed': 0, 'In Stock': 1, 'Upcoming': 2};
+
+    _state = state.copyWith(
+      appBarIndex: appBarIndex[menu]!,
+    );
+    notifyListeners();
+  }
+}
