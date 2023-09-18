1045984450fb1edd1654fa7c3bac7709a208d1be
blueknarr <knarrblue@gmail.com>
2023-09-18 15:50:48 +0900

    feat: search screen 라우팅에 따라 bottomnavbar 숨김 추가

---
+++ lib/presentation/main_bottom_navigation_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:snkr/presentation/main/main_view_model.dart';
+
+class MainBottomNavigationBar extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const MainBottomNavigationBar({
+    required this.navigationShell,
+    Key? key,
+  }) : super(key: key ?? const ValueKey<String>('MainBottomNavigationBar'));
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+    final state = viewModel.state;
+
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: state.isSearching
+          ? null
+          : BottomNavigationBar(
+              selectedItemColor: Colors.black,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              type: BottomNavigationBarType.fixed,
+              currentIndex: navigationShell.currentIndex,
+              onTap: (index) {
+                navigationShell.goBranch(index);
+              },
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  label: 'home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.explore_outlined),
+                  label: 'discover',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.email_outlined),
+                  label: 'email',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_outline),
+                  label: 'account',
+                ),
+              ],
+            ),
+    );
+  }
+}
