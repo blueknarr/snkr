7a6e9bc1e24313496a21636a18cd81bb51733c8c
blueknarr <knarrblue@gmail.com>
2023-09-18 15:52:35 +0900

    refactor: main view model 공유 및 아이템 single child scroll 변경

--- lib/presentation/discover/discover_screen.dart
+++ lib/presentation/discover/discover_screen.dart
@@ -1,23 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:snkr/presentation/discover/discover_view_model.dart';
 
-import '../../domain/model/product.dart';
+import '../main/main_view_model.dart';
+import 'components/event_items.dart';
+import 'components/product_items.dart';
 
 class DiscoverScreen extends StatelessWidget {
-  final List<Product> products;
-  const DiscoverScreen({Key? key, required this.products}) : super(key: key);
+  const DiscoverScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = context.watch<DiscoverViewModel>();
+    final viewModel = context.watch<MainViewModel>();
     final state = viewModel.state;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('${products.length}'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(40),
+        child: SafeArea(
+          child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    offset: const Offset(0, 2),
+                    blurRadius: 0.5,
+                    spreadRadius: 1.1,
+                  ),
+                ],
+              ),
+              child: const Center(
+                child: Text(
+                  'Discover',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )),
+        ),
       ),
-      body: Text('${state.events}'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Column(
+            children: [
+              EventItems(items: state.events, title: 'SNKRS Style'),
+              ProductItems(
+                  items: state.products.where((e) => e.brand == '나이키').toList(),
+                  title: '나이키'),
+              ProductItems(
+                  items:
+                      state.products.where((e) => e.brand == '아디다스').toList(),
+                  title: '아디다스'),
+              ProductItems(
+                  items: state.products.where((e) => e.brand == '휠라').toList(),
+                  title: '휠라'),
+              ProductItems(
+                  items:
+                      state.products.where((e) => e.brand == '뉴발란스').toList(),
+                  title: '뉴발란스'),
+              ProductItems(
+                  items: state.products.where((e) => e.brand == '푸마').toList(),
+                  title: '푸마'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
