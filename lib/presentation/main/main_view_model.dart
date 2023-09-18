7b1565d6fdbf414a4ea031e69c1d7d1b40768bed
blueknarr <knarrblue@gmail.com>
2023-09-18 15:56:41 +0900

    feat: 상품 상세 정보 bottom modal sheet

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/foundation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:snkr/core/result/result.dart';
 import 'package:snkr/domain/use_case/get_products_use_case.dart';
 import 'package:snkr/presentation/main/main_state.dart';
 
+import '../../domain/use_case/get_event_use_case.dart';
+import '../../domain/use_case/get_upcoming_use_case.dart';
+
+@singleton
 class MainViewModel with ChangeNotifier {
   final GetProductsUseCase _getProductsUseCase;
+  final GetEventUseCase _getEventUseCase;
+  final GetUpcomingUseCase _getUpcomingUseCase;
 
-  MainViewModel(this._getProductsUseCase) {
+  MainViewModel(
+    this._getProductsUseCase,
+    this._getEventUseCase,
+    this._getUpcomingUseCase,
+  ) {
     fetchProducts();
+    fetchEvents();
   }
 
   MainState _state = const MainState();
@@ -29,11 +41,40 @@ class MainViewModel with ChangeNotifier {
     }
   }
 
+  Future<void> fetchEvents() async {
+    final events = await _getEventUseCase.execute();
+
+    switch (events) {
+      case Success(:final data):
+        _state = state.copyWith(
+          events: data,
+        );
+        notifyListeners();
+      case Error(:final e):
+        _state = state.copyWith();
+        notifyListeners();
+    }
+  }
+
+  void changeSearchingStatus() {
+    _state = state.copyWith(
+      isSearching: !state.isSearching,
+    );
+    notifyListeners();
+  }
+
   void changeAppBarIndex(String menu) {
     Map<String, int> appBarIndex = {'Feed': 0, 'In Stock': 1, 'Upcoming': 2};
 
     _state = state.copyWith(
       appBarIndex: appBarIndex[menu]!,
+    );
+    notifyListeners();
+  }
+
+  void changeBottomNavigatorBarIndex(int index) {
+    _state = state.copyWith(
+      bottomNavigatorBarIndex: index,
     );
     notifyListeners();
   }
